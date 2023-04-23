ef72f58fe385106a87d3d348cc3e3c067a5d13fc
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-04-23 16:11:43 -0400

    Separacion de Useffect y control de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import { useEffect, useState } from 'react'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${FiestWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
 
 export const App = () => {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  const [error, setError] = useState()
 
+  // para recuperar la cita al cargar la pagnina
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
         const { fact } = data
         setFact(fact)
-        const firstWord = fact.split(' ', 3).join(' ')
-        console.log(firstWord)
-
-        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-          .then((res) => res.json())
-          .then((response) => {
-            const { url } = response
-            setImageUrl(`https://cataas.com${url}`)
-          })
       })
   }, [])
+
+  // para recuperar la imagen cada vez que tenemos una cita nueva
+
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response
+        setImageUrl(url)
+      })
+      .catch((err) => {
+        setError(err.message)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1>Gatitos</h1>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={` Image extracted using the first  threee words for ${fact} `} />}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={` Image extracted using the first  threee words for ${fact} `}
+        />
+      )}
+      {error && <p>{error}</p>}
     </main>
   )
 }
